ec384d9c6543e74c7a585ee9b2169b8408ab15e4
AbigaleJM <99094216+AbigaleJM@users.noreply.github.com>
2023-03-30 11:38:22 -0400

    Add files via upload

--- js/womensweb_js.js
+++ js/womensweb_js.js
@@ -1,31 +1,10 @@
 // JavaScript for Womens Web 
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    console.log("I am active")
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-      console.log("I am hidden")
-    } else {
-      content.style.display = "block";
-      console.log("I am displayed")
-    }
-  });
-}
+document.querySelector("#color").addEventListener("click",function() {
+  console.log("I am in color")
+  bd = document.querySelector("body")
+  bd.classList.toggle("scheme2")
+});
 
 
 
-
-
-// document.querySelector("collapsible").addEventListener("DOMContentLoaded",function() {
-//   console.log("I am collapsible")
-//   var myCollapse = document.getElementsByClassName("collapsible");
-//   myCollapse.addEventListener("hidden.bs.collapse", function(){
-//     console.log("I am collapsed")
-//   })
-// });
